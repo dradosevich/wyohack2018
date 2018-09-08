4a71300d84be12cd9ebb218b955d9deaecd50b9c
Damir Pulatov <31703384+damirpulat@users.noreply.github.com>
2018-09-08 12:09:49 -0600

    Add files via upload

--- back.py
+++ back.py
@@ -27,14 +27,18 @@ def update_history(days, num_points, curr1, curr2):
     jdata1 = requests.get(url1).json()
     jdata2 = requests.get(url2).json()
     
-    df1 = pd.DataFrame(jdata1['price'][-num_points:][1])
-    df2 = pd.DataFrame(jdata2['price'][-num_points:][1])
+    df1 = pd.DataFrame(jdata1['price'][-num_points:])
+    df2 = pd.DataFrame(jdata2['price'][-num_points:])
        
-    #Get relative value
-    df_relative = pd.DataFrame()
-    relative_value = df1 / df2
+    print(df1)
     
-    return relative_value
+    #Get relative value
+    relative_value = df1[1] / df2[1]
+    
+    df_rel = df1
+    df_rel[1] = relative_value
+     
+    return df_rel
     
 
 def compare(df, curr1, curr2):
@@ -51,13 +55,16 @@ def compare(df, curr1, curr2):
     return base_price, rel_price 
 
 
-def plot(data):
+def rel_plot(df):
+    plt.figure()
+    df.plot(x='0', y='1')
         
 
 if __name__ == '__main__':
     url = "http://coincap.io/front"
     df = update_front(url)
     base_price, rel_price = compare(df, "Bitcoin", "Ethereum")
-    relative_price = update_history(1, 10, 'BTC', 'DOGE')
-    
+    relative_df = update_history(1, 10, 'BTC', 'DOGE')
+    print(relative_df)
+    #plot(relative_df)
     
