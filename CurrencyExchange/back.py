034979dddb05bc4ad3c9698e30ae093d6bef3625
Wyatt <wyatt.k.emery@gmail.com>
2018-09-08 14:19:57 -0600

    added MA logic

--- CurrencyExchange/back.py
+++ CurrencyExchange/back.py
@@ -6,7 +6,7 @@ Backend for WyoHackathon2018
 
 import requests
 import pandas as pd
-import json 
+import json
 import matplotlib.pyplot as plt
 import technical_indicators as ti
 
@@ -24,52 +24,45 @@ def update_history(days, num_points, curr1, curr2):
     url = base_url + str(days) + "day/"
     url1 = url + curr1
     url2 = url + curr2
-    
+
     jdata1 = requests.get(url1).json()
     jdata2 = requests.get(url2).json()
-    
+
     df1 = pd.DataFrame(jdata1['price'][-num_points:])
     df2 = pd.DataFrame(jdata2['price'][-num_points:])
-    
+
     #Get relative value
     relative_value = df1[1] / df2[1]
-    
+
     df_rel = df1
     df_rel[1] = relative_value
-    
+
     df_rel = df_rel.rename(index=str, columns = {0: 'time', 1: 'price'})
     return df_rel
-    
+
+def get_currency_pairs():
+    url = "http://coincap.io/coins"
+    jdata = requests.get(url).json()
+    currency_list = json.loads(jdata)
+    return currency_list
 
 def compare(df, curr1, curr2):
     price1 = df.loc[df['long'] == curr1]
     price1 = price1['price'][0]
-    
+
     price2 = df.loc[df['long'] == curr2]
     price2 = price2['price'][1]
-    
+
     #Calculate relative price
     base_price = price1
     rel_price = price2 - base_price
-    
-    return base_price, rel_price 
+
+    return base_price, rel_price
 
 
 def rel_plot(df):
     df.plot(x='time', y='price')
 
-
-def ma_plot(df1, df2):
-    df1 = df1.reset_index()
-    df1.columns = ["Date","Open","High",'Low',"Close", "MA"]
-
-    df2 = df2.reset_index()
-    df2.columns = ["Date","Open","High",'Low',"Close", "MA"]
-    
-    ax = df1.plot(x='Date', y='MA')
-    df2.plot(ax=ax, x='Date', y='MA')
-    
-    
 
 def ma(df, n):
     ma_df = ti.moving_average(df, n)
@@ -80,25 +73,41 @@ def convert_data(df, timeframe):
     df['time'] = pd.to_datetime(df['time'],unit='ms')
     df = df.set_index(pd.DatetimeIndex(df['time']))
     data_ohlc =  df['price'].resample(timeframe).ohlc()
-    
+
     data_ohlc = data_ohlc.dropna()
-    
+
     data_ohlc = data_ohlc.rename(index=str, columns = {'time': 'Time', 'open': 'Open', 'low':'Low', 'high': 'High', 'close': 'Close'})
-    
+
     return data_ohlc
+
+def ma_crossover(slow_ma, fast_ma):
+    print(slow_ma.iloc[4,0])
+    if fast_ma.iloc[4,0] < slow_ma.iloc[4,0] and fast_ma.iloc[4,1] > slow_ma.iloc[4,1]:
+        #upward ma_crossover, buy signal
+        return "buy"
+    elif fast_ma.iloc[4,0] > slow_ma.iloc[4,0] and fast_ma.iloc[4,1] < slow_ma.iloc[4,1]:
+        #downward ma_crossover, sell signal
+        return "sell"
+    else:
+        return "none"
 
 if __name__ == '__main__':
     url = "http://coincap.io/front"
+
     df = update_front(url)
     base_price, rel_price = compare(df, "Bitcoin", "Ethereum")
     relative_df = update_history(1, 100, 'BTC', 'DOGE')
     rel_plot(relative_df)
-    
+
     converted_data = convert_data(relative_df, '15Min')
     #print(converted_data)
     slow_ma = ma(converted_data, 20)
     fast_ma = ma(converted_data, 10)
-    #print(ma_df)
-    
-    ma_plot(slow_ma, fast_ma)
-    
+
+
+    #this will go in a loop of some sort
+    # result = ma_crossover(slow_ma, fast_ma)
+    # if result is "buy" or "sell":
+    #     #save cypto pair in list or some such
+    # else:
+    #     continue
